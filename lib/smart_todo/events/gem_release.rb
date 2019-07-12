9a7697da51a2e289b04c12269d5b12d8adad1e68
Edouard CHIN <edouard.chin@shopify.com>
2019-07-12 02:34:35 +0200

    Modify the `GemReleaseEvent` to accept requirements instead:
    - Instead of expecting a specific version, it's more useful to have
      a list of version requirements.
    
      A user might not know if the next release if gonna be a bump,
      a minor or a major. By using requirements version, this is more
      flexible.

--- lib/smart_todo/events/gem_release.rb
+++ lib/smart_todo/events/gem_release.rb
@@ -6,9 +6,9 @@ require 'json'
 module SmartTodo
   module Events
     class GemRelease
-      def initialize(gem_name, version)
+      def initialize(gem_name, requirements)
         @gem_name = gem_name
-        @version = version
+        @requirements = Gem::Requirement.new(requirements)
       end
 
       def met?
@@ -16,8 +16,8 @@ module SmartTodo
 
         if response.code_type < Net::HTTPClientError
           error_message
-        elsif version_released?(response.body)
-          message
+        elsif (gem = version_released?(response.body))
+          message(gem['number'])
         else
           false
         end
@@ -27,14 +27,16 @@ module SmartTodo
         "The gem *#{@gem_name}* doesn't seem to exist, I can't determine if your TODO is ready to be addressed."
       end
 
-      def message
-        "The gem *#{@gem_name}* was released to version *#{@version}* and your TODO is now ready to be addressed."
+      def message(version_number)
+        "The gem *#{@gem_name}* was released to version *#{version_number}* and your TODO is now ready to be addressed."
       end
 
       private
 
       def version_released?(gem_versions)
-        JSON.parse(gem_versions).find { |gem| gem['number'] == @version }
+        JSON.parse(gem_versions).find do |gem|
+          @requirements.satisfied_by?(Gem::Version.new(gem['number']))
+        end
       end
 
       def client
