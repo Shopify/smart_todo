4e5b3c4b0f06875994b966fd1db1554596b71049
Edouard CHIN <edouard.chin@shopify.com>
2019-07-12 02:02:10 +0200

    Create the on_pull_request_close/on_issue_closed event:
    - User will be able to be warned when a todo related to the release
      of a gem comes to expiration.
    
      A concrete example is to have to write a monkeypatch in your app
      and have to be reminded to remove it when a gem is released with
      a new version that has the feature/bug fix you need.
    
      The declaration is as follow
    
      ```ruby
        # @smart_todo on_pull_request_close('rails', 'rails', '123')
        # @smart_todo on_issue_close('rails', 'rails', '123')
      ```
    
      If the repo is a private one the user has to export a GitHub token
      with the `repo` scope.

--- lib/smart_todo.rb
+++ lib/smart_todo.rb
@@ -18,5 +18,6 @@ module SmartTodo
   module Events
     autoload :Date,                   'smart_todo/events/date'
     autoload :GemRelease,             'smart_todo/events/gem_release'
+    autoload :PullRequestClose,       'smart_todo/events/pull_request_close'
   end
 end
--- lib/smart_todo/events.rb
+++ lib/smart_todo/events.rb
@@ -11,5 +11,10 @@ module SmartTodo
     def on_gem_release(gem_name, version)
       GemRelease.new(gem_name, version).met?
     end
+
+    def on_pull_request_closed(organization, repo, pr_number)
+      PullRequestClosed.new(organization, repo, pr_number).met?
+    end
+    alias_method :on_issue_close, :on_pull_request_closed
   end
 end
