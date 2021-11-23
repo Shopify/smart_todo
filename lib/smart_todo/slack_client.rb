a9c292ce685fd076bc0b085da31cf595516d1cb2
Andrew Kerr <andrew.kerr@shopify.com>
2021-11-23 10:42:10 -0500

    Prevent crash when malformed emails are present

--- lib/smart_todo/slack_client.rb
+++ lib/smart_todo/slack_client.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "cgi"
 require "net/http"
 require "json"
 
@@ -37,14 +38,14 @@ module SmartTodo
     # @param email [String]
     # @return [Hash]
     #
-    # @raise [Net::HTTPError] in case the reques to Slack failed
-    # @raise [SlackClient::Error] in case Slack returs a { ok: false } in the body
+    # @raise [Net::HTTPError] in case the request to Slack failed
+    # @raise [SlackClient::Error] in case Slack returns a { ok: false } in the body
     #
     # @see https://api.slack.com/methods/users.lookupByEmail
     def lookup_user_by_email(email)
       headers = { "Content-Type" => "application/x-www-form-urlencoded" }
 
-      request(:get, "/api/users.lookupByEmail?email=#{email}", nil, headers)
+      request(:get, "/api/users.lookupByEmail?email=#{CGI.escape(email)}", nil, headers)
     end
 
     # Send a message to a Slack channel or to a user
@@ -53,8 +54,8 @@ module SmartTodo
     # @param text [String] The message to send
     # @return [Hash]
     #
-    # @raise [Net::HTTPError] in case the reques to Slack failed
-    # @raise [SlackClient::Error] in case Slack returs a { ok: false } in the body
+    # @raise [Net::HTTPError] in case the request to Slack failed
+    # @raise [SlackClient::Error] in case Slack returns a { ok: false } in the body
     #
     # @see https://api.slack.com/methods/chat.postMessage
     def post_message(channel, text)
@@ -68,8 +69,8 @@ module SmartTodo
     # @param data [String] JSON encoded data when making a POST request
     # @param headers [Hash]
     #
-    # @raise [Net::HTTPError] in case the reques to Slack failed
-    # @raise [SlackClient::Error] in case Slack returs a { ok: false } in the body
+    # @raise [Net::HTTPError] in case the request to Slack failed
+    # @raise [SlackClient::Error] in case Slack returns a { ok: false } in the body
     def request(method, endpoint, data = nil, headers = {})
       response = case method
       when :post, :patch
@@ -87,8 +88,8 @@ module SmartTodo
     #   (Net::HTTPOK, Net::HTTPNotFound ...)
     # @return [Hash]
     #
-    # @raise [Net::HTTPError] in case the reques to Slack failed
-    # @raise [SlackClient::Error] in case Slack returs a { ok: false } in the body
+    # @raise [Net::HTTPError] in case the request to Slack failed
+    # @raise [SlackClient::Error] in case Slack returns a { ok: false } in the body
     def slack_response!(response)
       raise(Net::HTTPError.new("Request to slack failed", response)) unless response.code_type < Net::HTTPSuccess
       body = JSON.parse(response.body)
