d932a66fec84ddc8f39d1cfa756776f8f3b23762
Andrew Kerr <andrew.kerr@shopify.com>
2021-11-23 09:25:37 -0500

    Fix ArgumentError in output dispatcher

---
+++ test/smart_todo/dispatchers/output_test.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module SmartTodo
+  module Dispatchers
+    class OutputTest < Minitest::Test
+      def setup
+        @options = { fallback_channel: "#general", slack_token: "123" }
+      end
+
+      def test_dispatch
+        dispatcher = Output.new("Foo", todo_node, "file.rb", @options)
+        expected_text = <<~HEREDOC
+          Hello :wave:,
+
+          You have an assigned TODO in the `file.rb` file.
+          Foo
+
+          Here is the associated comment on your TODO:
+
+          ```
+
+          ```
+        HEREDOC
+
+        assert_output(expected_text) { dispatcher.dispatch }
+      end
+
+      private
+
+      def todo_node(*assignees)
+        tos = assignees.map { |assignee| "to: '#{assignee}'" }
+        tos << "to: 'john@example.com'" if assignees.empty?
+
+        ruby_code = <<~EOM
+          # TODO(on: date('2011-03-02'), #{tos.join(", ")})
+          def hello
+          end
+        EOM
+
+        Parser::CommentParser.new(ruby_code).parse[0]
+      end
+    end
+  end
+end
